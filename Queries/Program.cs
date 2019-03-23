8b36e865c22433e2afdf4b94709dd10bd002c2bf
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-22 20:35:24 -0500

    displaying the count. added the ToList ext to reduce redundency.

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -20,7 +20,10 @@ namespace Queries
 
             //using extension method syntax && implementing deferred execution...i.e. query does no real work until we force it to produce a result
             //defining the query...
-            var query = movies.Filter(m => m.Year > 2000);
+            var query = movies.Filter(m => m.Year > 2000).ToList();
+            //demonstating how deferred execution can be bad...looping through once to get the count and then through the same list again to get the titles
+            //adding ToList to the query creates a separate list that reduces redundency
+            Console.WriteLine(query.Count());
 
             //USING ENUMERATOR WHILE LOOP INSTEAD OF A FOREACH
             var enumerator = query.GetEnumerator();
